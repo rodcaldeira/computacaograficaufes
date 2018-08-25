19700db3ad5177028639a35e4529ac721d192d1d
Rodrigo Caldeira <rod.cdr@gmail.com>
2018-08-25 18:20:51 -0300

    starting tc2

---
+++ trab2/main.cpp
@@ -0,0 +1,16 @@
+// trabalho  1
+// para referencia futuras em caso de xml mais complexos
+// https://shilohjames.wordpress.com/2014/04/27/tinyxml2-tutorial/
+
+#include <cstddef>
+#include <iostream>
+#include <sstream>
+#include <list>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <math.h>
+#include "tinyxml2/tinyxml2.h"
+#define PI 3.1415926535897932384626433832795
+using namespace std;
